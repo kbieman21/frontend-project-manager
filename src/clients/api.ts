8523d0df00295cb4dff8dc0e24a92f139f905dbf
Kibreab Solomon <kbieman21@yahoo.com>
2025-12-05 15:35:02 -0700

    update token

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
     headers:{
-        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzFjYjg0NzViYTYwZGZmNDVkM2NjZiIsInVzZXJuYW1lIjoidXNlcjExMSIsImVtYWlsIjoidXNlcjExMUB0ZXN0LmNvbSIsInJvbGUiOiJ1c2VyIn0sImlhdCI6MTc2NDk1Njg1MywiZXhwIjoxNzY0OTY0MDUzfQ.7zCE6-5GRJ1JIuf0q2SavtUbs_lHJb1tCxNFiv9UIrA"
+        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzFjYjg0NzViYTYwZGZmNDVkM2NjZiIsInVzZXJuYW1lIjoidXNlcjExMSIsImVtYWlsIjoidXNlcjExMUB0ZXN0LmNvbSIsInJvbGUiOiJ1c2VyIn0sImlhdCI6MTc2NDk3NDAyNywiZXhwIjoxNzY1MDYwNDI3fQ.Q93K-44UYN0mY1LZY2rj4Xjk4vM5w-mA-OKSIBleSF8"
     }
 });
